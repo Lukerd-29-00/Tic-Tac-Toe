143fc522b1783deb0f80a0814a94392ad72275e4
lucasd <Driscoll_Lucas@bah.com>
2021-09-28 16:38:43 -0400

    Added tic-tac-toe page

--- src/tic-tac-toe/components/TicTacToeDashboard.tsx
+++ src/tic-tac-toe/components/TicTacToeDashboard.tsx
@@ -1,19 +1,21 @@
 import { TicTacToeBoard } from "./TicTacToeBoard";
-import { Inactive } from "../inactive/Inactive";
-import { DashboardProps } from "../../App";
 
-export function TicTacToeDashboard(props: DashboardProps){
-    return props.active ? (
+
+interface noprops {
+
+}
+export function TicTacToeDashboard(props: noprops){
+    return (
         <section>
             <div>
                 <TicTacToeBoard />
             </div>
             <footer>
-                <button className="small-link" onClick={props.disable}>description</button>
+                <p><a href="https://lukerd-29-00.github.io/Portfolio/">Homepage</a></p>
+                <p><a href="https://github.com/Lukerd-29-00/Tic-Tac-Toe">Tic-Tac-Toe source code</a></p>
+                <p><a href="https://github.com/Lukerd-29-00/Portfolio">Main repository</a></p>
             </footer>
         </section>
 
-    ) : (
-        <Inactive enabler = {props.enable}/>
     )
 }
