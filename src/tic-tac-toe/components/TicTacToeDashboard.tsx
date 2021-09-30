292fdffcb02e95bc7c971c45f38591e521625876
lucasd <Driscoll_Lucas@bah.com>
2021-09-30 16:56:54 -0400

    Updated source code link

--- src/tic-tac-toe/components/TicTacToeDashboard.tsx
+++ src/tic-tac-toe/components/TicTacToeDashboard.tsx
@@ -12,7 +12,7 @@ export function TicTacToeDashboard(props: noprops){
             </div>
             <footer>
                 <p><a href="https://lukerd-29-00.github.io/Portfolio/">Homepage</a></p>
-                <p><a href="https://github.com/Lukerd-29-00/Tic-Tac-Toe">Tic-Tac-Toe source code</a></p>
+                <p><a href="https://github.com/Lukerd-29-00/Tic-Tac-Toe/tree/source/src/tic-tac-toe">Source code</a></p>
                 <p><a href="https://github.com/Lukerd-29-00/Portfolio">Main repository</a></p>
             </footer>
         </section>
